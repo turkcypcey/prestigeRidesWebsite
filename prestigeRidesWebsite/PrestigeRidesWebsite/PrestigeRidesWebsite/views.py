79a49a007b34f1af4f70118cd29c940f8b034a01
turkcypcey <cc835@canterbury.ac.uk>
2022-05-18 00:36:15 +0000

    set up of repo and start of home page and navbar

---
+++ prestigeRidesWebsite/PrestigeRidesWebsite/PrestigeRidesWebsite/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+def index(request):
+    return render( request, 'index.html')
+
+def aboutUs(request):
+    return render( request, 'aboutUs.html')
+
+def brands(request):
+    return render( request, 'brands.html')
+
+def contactUs(request):
+    return render( request, 'contactUs.html')
+
+   
